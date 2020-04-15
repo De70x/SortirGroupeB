b8a93d2440e23f2464c9b4cec5e63976676be39d
Lysergik4 <arthur85300@hotmail.fr>
2020-04-15 12:04:21 +0200

    Commit méthode Register dans le UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,17 +10,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function login() {
+    public function login(AuthenticationUtils $authUtils) {
 
-        return $this->render('user/login.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $error = $authUtils->getLastAuthenticationError();
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
 
     /**
