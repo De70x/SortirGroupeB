6de1557d1164d3b7fd34a7b274691e2614638f4c
Lysergik4 <arthur85300@hotmail.fr>
2020-04-15 10:21:39 +0200

    Commit méthode Register dans le UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,19 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Entity\User;
 use App\Form\UserFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function login()
-    {
+    public function login() {
+
         return $this->render('user/login.html.twig', [
             'controller_name' => 'UserController',
         ]);
@@ -24,17 +26,32 @@ class UserController extends AbstractController
     /**
      * @Route("/register", name="register")
      */
-    public function register(Request $request, EntityManagerInterface $em) {
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder) {
 
-        $user = new Participant();
+        $user = new User();
         $userForm = $this->createForm(UserFormType::class, $user);
 
         $userForm->handleRequest($request);
         if ($userForm->isSubmitted() && $userForm->isValid()) {
 
+            $hashed = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hashed);
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash("success", "Votre compte à bien été créé ");
         }
         return $this->render('user/register.html.twig', [
-            'controller_name' => 'UserController',
+            'userForm' => $userForm->createView()
         ]);
     }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(){}
+
+
+
 }
