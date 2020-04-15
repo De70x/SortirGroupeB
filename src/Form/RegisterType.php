bed23b8623c0b70860194edc1075d81f2ffa42c0
Louis <louis.barre.info@gmail.com>
2020-04-15 12:24:40 +0200

    maj de l'entity User v2 plus creation RegisterForm

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use phpDocumentor\Reflection\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prenom'
+            ])
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur'
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('mail', TextType::class, [
+                'label' => 'Email'
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'les deux mots de passes doivent être identique !',
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répéter le mot de passe']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
