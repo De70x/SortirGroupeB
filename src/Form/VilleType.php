ec1d82b438223f772832bb49a00d06f334a4c6cc
Louis <louis.barre.info@gmail.com>
2020-04-16 15:35:15 +0200

    ajout du formulaire "Ajouter une ville"

--- src/Form/VilleType.php
+++ src/Form/VilleType.php
@@ -2,26 +2,22 @@
 
 namespace App\Form;
 
-use App\Entity\Lieu;
+use App\Entity\Ville;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class NewLieuType extends AbstractType
+class VilleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('nom', TextType::class,[
-                'label'=>'Nom du lieu'
+                'label'=>'Ville'
             ])
-            ->add('rue', TextType::class,[
-                'label'=>'Adresse'
-            ])
-            ->add('ville',  null,[
-                'label'=>'Ville',
-                'choice_label'=>'nom'
+            ->add('codePostal', TextType::class,[
+                'label'=>'Code Postal'
             ])
         ;
     }
@@ -29,7 +25,7 @@ class NewLieuType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Lieu::class,
+            'data_class' => Ville::class,
         ]);
     }
 }
