b5ee7f9a1e0cf22fb5259c7d4f22260699c0918e
Misterlenon <33966612+Misterlenon@users.noreply.github.com>
2020-04-20 14:49:01 +0200

    fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,17 +6,26 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
+
         for ($i = 0; $i < 5; $i++) {
             $user = new User();
             $user->setNom("Michel".$i);
             $user->setPrenom("Michel".$i);
             $user->setUsername("peusdo".$i);
-            $user->setPassword("mdp");
+            $hashed = $this->encoder->encodePassword($user,'mdp');
+            $user->setPassword($hashed);
             $user->setTelephone("0642424242");
             $user->setMail("mail".$i."@michel.com");
             $user->setAdministrateur(false);
