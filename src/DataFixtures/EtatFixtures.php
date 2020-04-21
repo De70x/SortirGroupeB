b3c07f9d827432cdf35b8d4ca3aad072899952c4
adeloffre <antoine.deloffre2018@ampus-eni.fr>
2020-04-21 09:44:09 +0200

    changement sur les fixtures pour avoir des données cohérentes et variées

--- src/DataFixtures/EtatFixtures.php
+++ src/DataFixtures/EtatFixtures.php
@@ -14,35 +14,35 @@ class EtatFixtures extends Fixture {
     public function load(ObjectManager $manager)
     {
         $i=0;
-            $etat = new Etat();
+            $etat1 = new Etat();
             $etat2 = new Etat();
             $etat3 = new Etat();
             $etat4 = new Etat();
             $etat5 = new Etat();
             $etat6 = new Etat();
 
-            $etat->setLibelle(Etat::CREEE);
-            $this->setReference('CREEE',$etat);
-            $manager->persist($etat);
+            $etat1->setLibelle(Etat::CREEE);
+            $this->setReference('Etat1',$etat1);
+            $manager->persist($etat1);
 
             $etat2->setLibelle(Etat::OUVERTE);
-            $this->setReference('OUVERTE',$etat2);
+            $this->setReference('Etat2',$etat2);
             $manager->persist($etat2);
 
             $etat3->setLibelle(Etat::CLOTUREE);
-            $this->setReference('CLOTUREE',$etat3);
+            $this->setReference('Etat3',$etat3);
             $manager->persist($etat3);
 
             $etat4->setLibelle(Etat::ANNULEE);
-            $this->setReference('ANNULEE',$etat4);
+            $this->setReference('Etat4',$etat4);
             $manager->persist($etat4);
 
             $etat5->setLibelle(Etat::EN_COURS);
-            $this->setReference('EN COURS',$etat6);
+            $this->setReference('Etat5',$etat6);
             $manager->persist($etat5);
 
             $etat6->setLibelle(Etat::PASSEE);
-            $this->setReference('PASSSEE',$etat6);
+            $this->setReference('Etat6',$etat6);
             $manager->persist($etat6);
 
         $manager->flush();
