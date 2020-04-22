7909b94b81d444f767a403cd3bf668e103d99607
Misterlenon <33966612+Misterlenon@users.noreply.github.com>
2020-04-22 17:33:24 +0200

    commit v2 de la page des détails

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -22,16 +22,21 @@ class UserFixtures extends Fixture implements DependentFixtureInterface
 
         for ($i = 0; $i < 5; $i++) {
             $user = new User();
-            $idSite = rand(0,4);
-            $user->setNom("Michel".$i);
-            $user->setPrenom("Michel".$i);
-            $user->setUsername("peusdo".$i);
-            $hashed = $this->encoder->encodePassword($user,'mdp');
-            $user->setSite($this->getReference('site'.$idSite));
+            $idSite = rand(0, 4);
+            $user->setNom("Michel" . $i);
+            $user->setPrenom("Michel" . $i);
+            $user->setUsername("peusdo" . $i);
+            $hashed = $this->encoder->encodePassword($user, 'mdp');
+            $user->setSite($this->getReference('site' . $idSite));
             $user->setPassword($hashed);
             $user->setTelephone("0642424242");
-            $user->setMail("mail".$i."@michel.com");
+            $user->setMail("mail" . $i . "@michel.com");
+            if ($i = 0) {
+                $user->setAdministrateur(true);
+            } else {
+
             $user->setAdministrateur(false);
+            }
             $this->addReference('user'.$i,$user);
 
 
